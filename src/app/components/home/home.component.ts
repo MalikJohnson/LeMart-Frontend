4eacc58b29e651841f2e3be85039641740dd0791
Malik Johnson <60333731+MalikJohnson@users.noreply.github.com>
2025-04-02 04:10:13 -0400

    Empty Search Brings Up All Products
    Before clicking search on an empty search bar did nothing. Now when an empty search is done it brings up all products.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -87,6 +87,8 @@ export class HomeComponent implements OnInit {
     if (this.searchQuery.trim()) {
       this.selectedCategoryId = null;
       this.loadProducts();
+    } else {
+      this.clearFilters(); 
     }
   }
 
